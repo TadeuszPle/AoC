5fff59fdde05407d39d0b4a69d7152cc41d5f232
tp <katnak56@gmail.com>
2024-01-03 18:53:23 +0100

    18 part 1 naive solution

--- 18/1.py
+++ 18/1.py
@@ -1,4 +1,5 @@
-input_file = 'input2.txt'
+import queue
+input_file = 'input.txt'
 raw_lines = open(input_file).read().strip().split('\n')
 instructions = [(line.split()[0], int(line.split()[1]), line.split()[2][1:-1]) for line in raw_lines]
 directions = {
@@ -20,6 +21,21 @@ def get_grid_size(instrs):
         min_w = min(min_w, cur_pos[1])
     return min_h, min_w, max_h, max_w
 
+def flood_fill(grid, start_point):
+    q = [start_point]
+    while q:
+        y, x  = q.pop()
+    # stop if at edge or out of bounds:
+        if x < 0 or x > len(grid[y]) or y < 0 or y > len(grid) or grid[y][x] != 0:
+            continue
+        # mark as visited
+        grid[y][x] = 3
+        q.append((y, x+1) )
+        q.append((y, x-1) )
+        q.append((y+1, x) )
+        q.append((y-1, x) )
+    return grid  
+
 def get_area(grid):
     area = 0
     for line in grid:
@@ -36,30 +52,45 @@ def get_area(grid):
         if last_one:
             area += 1
     return area
+def get_area_simple(grid):
+    area = 0
+    for line in grid:
+        for i, c in enumerate(line):
+            if c != 0:
+                area += 1
+    return area
+def draw_boudaries(instructions, min_h, min_w, max_h, max_w):
+    width = max_w - min_w + 1
+    height = max_h - min_h + 1
+    print(width, height)
+    grid = [[0]*width for _ in range(height)]
+    start_pos = (-min_h, -min_w)
+    cur_pos = start_pos
+    grid[start_pos[0]][start_pos[1]] = 2
+    for ins_ind, (letter, le, col) in enumerate(instructions):
+        
+        cur_dir = directions[letter]
+        y,x = cur_pos
+        d = (cur_dir[0]*le, cur_dir[1]*le)
+        # print(ins_ind+1, y,x,d)
+        if letter == 'D' or letter == 'U':
+            for i in range(1,le+1):
+                grid[y+i*cur_dir[0]][x] = 1
+        else:
+            for i in range(1,le+1):
+                grid[y][x+i*cur_dir[1]] = 1
+        
+        cur_pos = y + d[0], x + d[1]
+    return grid
 
 min_h, min_w, max_h, max_w = get_grid_size(instructions)
-width = max_w - min_w + 1
-height = max_h - min_h + 1
-print(width, height)
-grid = [[0]*width for _ in range(height)]
-start_pos = (-min_h, -min_w)
-cur_pos = start_pos
-grid[start_pos[0]][start_pos[1]] = 2
-for ins_ind, (letter, le, col) in enumerate(instructions):
-    
-    cur_dir = directions[letter]
-    y,x = cur_pos
-    d = (cur_dir[0]*le, cur_dir[1]*le)
-    print(ins_ind+1, y,x,d)
-    if letter == 'D' or letter == 'U':
-        for i in range(1,le+1):
-            grid[y+i*cur_dir[0]][x] = 1
-    else:
-        for i in range(1,le+1):
-            grid[y][x+i*cur_dir[1]] = 1
-    
-    cur_pos = y + d[0], x + d[1]
-print(get_area(grid=grid))
+grid = draw_boudaries(instructions, min_h, min_w, max_h, max_w)
+grid[-min_h ][-min_w] = 2
+print(-min_h ,-min_w)
+grid = flood_fill(grid, (-min_h+1, -min_w+1))
+g_area = get_area_simple(grid)
+print(g_area)
+
 with open('grid.txt', 'w') as f:
     for line in grid:
         for i in line:
